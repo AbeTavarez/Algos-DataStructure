169fe12171e3efa46e953066e43c12b38b9d1c7e
Abraham E. Tavarez <abeincwireless@gmail.com>
2022-04-10 20:03:01 -0400

    reverse string exercise

---
+++ 1-reverseString/index.js
@@ -0,0 +1,24 @@
+//* ===== Directions =====
+//* - Write a function that takes a string as an input,
+//* - and return the a new string with the reversed order of characters.
+
+//* ===== Examples =====
+//* reverseStr('hello') === 'olleh'
+//* reverseStr('apple') === 'leppa'
+
+
+//* @param str === input string. 
+
+function reverseStr(str){
+    // TODO: Write your code below this line
+
+};
+
+
+
+
+
+
+
+
+//? Hints: don't forget to return the new string.
