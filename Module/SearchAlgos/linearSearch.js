9f1d02593008c066ab7406c1d0bade1606a8c830
Abraham E. Tavarez <abetavarez@icloud.com>
2022-08-22 22:34:37 -0400

    linear search

---
+++ Module/SearchAlgos/linearSearch.js
@@ -0,0 +1,20 @@
+const array = [1, 3984, 4, 73, 6, 857, 92, 837, 95];
+
+// Create a function that takes in a target' value and an array to search to the value.
+
+// The input array must is unordered
+
+const linearSearch = (arr, target) => {
+  for (num of arr) {
+    if (target === num) {
+      return true;
+    }
+  }
+  return false;
+};
+
+const result = linearSearch(array, 5); //false
+const found = linearSearch(array, 92);
+
+console.log(result);
+console.log(found);
